6c73e14853965731478166707c2ba3d491729afc
gubusre <g96busquets@gmail.com>
2026-03-06 17:49:19 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -8,9 +8,48 @@ typedef struct s_mshell
     int     exit_code;
 }   t_mshell;
 
+
+/**************************************************************Loop Shell*******************************************************************/
+
+
+
 /**************************************************************Init Shell*******************************************************************/
 
+static char    **dup_env(char *env[])
+{
+    char    *new_env[];
+    int     i;
 
+    i = 0;
+    while (env[i])
+        i++;
+    new_env = malloc(sizeof(char *) * i + 1);
+    if (!new_env)
+        return (NULL);
+    i = 0;
+    while (env[i])
+    {
+        new_env[i] = ft_strdup(env[i]);
+        i++;
+    }
+    new_env[i] = '\0';
+    return (new_env);
+}
+
+/*Used to save the enviroment variables (why?, ex: at the case there is and unset of one of those variables, y can still find it's locations (supongo)*/
+static int init_shell(t_mshell *shell, char *env[], int argc)
+{
+    if (argc != 1)
+    {
+        write(1, "Error: no arguments allowed\n", 28);
+        return (1);
+    }
+    shell->exit_code = 0;
+    shell->env = dup_env(env);
+    if (!shell->env)
+        return (1);
+    return (0);
+}
 /**************************************************************Main*************************************************************************/
 
 int g_signal = 0;
@@ -34,9 +73,9 @@ static void steup_signals(void)
 
 int main(int argc, char *argv[], char *env[])
 {
-    t_mshell mshell;
+    t_mshell shell;
 
-    if (init_shell(argc, env, &mshell))
+    if (init_shell(argc, env, &shell))
         return(1);
     setup_signals();
     loop_shell
